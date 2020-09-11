2a244e3526c6827c94185bfafad0be4d83a16847
pcrandall <crandall.phillip@gmail.com>
2020-09-11 18:18:26 -0500

    sorted aisles

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"sort"
 
 	"github.com/360EntSecGroup-Skylar/excelize"
 )
@@ -12,10 +13,10 @@ import (
 // sort by x coord
 // write to xlsx
 type floor struct {
-	level_1 [][]string
-	level_2 [][]string
-	level_3 [][]string
-	level_4 [][]string
+	f1 [][]string
+	f2 [][]string
+	f3 [][]string
+	f4 [][]string
 }
 
 func main() {
@@ -39,34 +40,56 @@ func main() {
 		// for _, colCell := range row {
 		// 	fmt.Print(colCell, "\t")
 		// }
-		// fmt.Println("whole row", row)
-		// fmt.Println("index", row[1])
 		level(row[1], flr, row)
 	}
-	fmt.Println("first floor\n", flr.level_1, "\n",
-		"second floor\n", flr.level_2, "\n",
-		"third floor\n", flr.level_3, "\n",
-		"fourth floor\n", flr.level_4, "\n")
+	// fmt.Println("first floor\n", flr.f1, "\n",
+	// 	"second floor\n", flr.f2, "\n",
+	// 	"third floor\n", flr.f3, "\n",
+	// 	"fourth floor\n", flr.f4, "\n")
+
+	sort.Slice(flr.f1, func(j, k int) bool {
+		// J, _ := strconv.Atoi(flr.f1[j][0])
+		// K, _ := strconv.Atoi(flr.f1[k][0])
+		// fmt.Println("J", flr.f1[j][0], "K", flr.f1[k][0])
+		fmt.Println("J", flr.f1[j][1], "K", flr.f1[k][1], flr.f1[j][1] < flr.f1[j][1])
+		return flr.f1[j][1] > flr.f1[k][1]
+	})
+	sort.Slice(flr.f2, func(j, k int) bool {
+		return flr.f2[j][1] > flr.f2[k][1]
+	})
+	sort.Slice(flr.f3, func(j, k int) bool {
+		return flr.f3[j][1] > flr.f3[k][1]
+	})
+	sort.Slice(flr.f4, func(j, k int) bool {
+		return flr.f4[j][1] > flr.f4[k][1]
+	})
+
+	fmt.Println("first floor\n", flr.f1, "\n",
+		"second floor\n", flr.f2, "\n",
+		"third floor\n", flr.f3, "\n",
+		"fourth floor\n", flr.f4, "\n")
+
 }
 
-func level(s string, f *floor, r []string) {
+func level(s string, flr *floor, r []string) {
 
 	//NRA4103X66240Y02Z25
 	// Floor is always going to be the 7th char
 	sn := string(s[6])
+	fmt.Println(sn)
 	switch sn {
 	case "1":
 		fmt.Println("First floor", s)
-		f.level_1 = append(f.level_1, r)
+		flr.f1 = append(flr.f1, r)
 	case "2":
 		fmt.Println("Second floor", s)
-		f.level_2 = append(f.level_2, r)
+		flr.f2 = append(flr.f2, r)
 	case "3":
 		fmt.Println("Third floor", s)
-		f.level_3 = append(f.level_3, r)
+		flr.f3 = append(flr.f3, r)
 	case "4":
 		fmt.Println("Fourth floor", s)
-		f.level_4 = append(f.level_4, r)
+		flr.f4 = append(flr.f4, r)
 	}
 }
 
